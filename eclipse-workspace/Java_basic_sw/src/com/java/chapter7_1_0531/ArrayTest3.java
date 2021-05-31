d2dbc6ec9b68cc2b0971eee7a23e4cdd30657e5a
SungWoo0315 <noel7752@gmail.com>
2021-05-31 11:40:54 +0900

    Create ArrayTest3.java

---
+++ eclipse-workspace/Java_basic_sw/src/com/java/chapter7_1_0531/ArrayTest3.java
@@ -0,0 +1,40 @@
+package com.java.chapter7_1_0531;
+
+public class ArrayTest3 {
+
+	public static void main(String[] args) {
+		
+		double[] data = new double[5];
+		//이런 방식으로 바로 쓰지말고,
+		for (int i = 0; i < data.length; i++) {
+			System.out.println(data[i]);
+			System.out.println(data[i] + 1);
+		}
+		
+		// 이렇게 전체 배열크기만큼 초기화 하는것이 좋다.
+		for (int i = 0; i < data.length; i++) {
+			data[i] = -1.0;
+			
+//			System.out.println(data[i]);
+//			System.out.println(data[i] + 1);
+			
+		}
+		
+		int index = 0;
+		//추가
+		data[0] = 10.0; index++;
+		data[1] = 11.0;	index++;
+		data[2] = 0.5;	index++;
+		
+		//삭제
+		data[2] = -1.0; index--;
+		
+		System.out.println("배열 " + data.length + "인덱스 : " + index);
+		
+		for (int i = 0; i < index; i++) {
+			System.out.println(data[i]);
+		}
+		
+	}
+
+}
