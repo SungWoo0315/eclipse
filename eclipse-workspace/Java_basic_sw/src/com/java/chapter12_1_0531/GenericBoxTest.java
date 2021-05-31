7e4819adb580abbcd510ed045e51afb32bdaa4d2
SungWoo0315 <noel7752@gmail.com>
2021-05-31 11:40:45 +0900

    Create GenericBoxTest.java

---
+++ eclipse-workspace/Java_basic_sw/src/com/java/chapter12_1_0531/GenericBoxTest.java
@@ -0,0 +1,45 @@
+package com.java.chapter12_1_0531;
+
+public class GenericBoxTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Box box = new Box();
+		box.set("안녕");
+		box.set( new Integer(100));
+		//
+		String str = (String)box.get();
+		
+		Box2<String> box2 = new Box2();
+		Box2<String> box3 = new Box2<String>();
+		
+		box2.set("안녕");
+		str = box2.get();
+		
+	}
+	
+
+}
+
+
+
+class Box {
+	private Object obj;
+	public void set(Object o) { 
+		this.obj = o;
+	}
+	public Object get()	 {
+		return this.obj;
+	}
+}
+
+
+class Box2<String> {
+	private String str;
+	public void set(String s) { 
+		this.str = s;
+	}
+	public String get()	 {
+		return this.str;
+	}
+}
