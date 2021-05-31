2370da66ccbf8e7ccb9e62c8de74352ba600152e
SungWoo0315 <noel7752@gmail.com>
2021-05-31 11:40:38 +0900

    Update ArrayTest1.java

--- eclipse-workspace/Java_basic/src/com/java/ex/day6/ArrayTest1.java
+++ eclipse-workspace/Java_basic/src/com/java/ex/day6/ArrayTest1.java
@@ -1,12 +1,77 @@
 package com.java.ex.day6;
 
+import com.java.homework.test;
+
 public class ArrayTest1 {
 
 	public static void main(String[] args) {
 		// 10개 짜리 문자열 배열
 		// 1번
 		String[] testArray;
-		testArray = new String[10];
+		testArray = new String[10]; // 초기화, null 
+		
+		System.out.println(testArray[0]);
+		
+// 2021년 5월 31일 범위 시작.
+		for(int i = 0; i < testArray.length; i++) {
+			System.out.println(testArray[i]); // 배열 탐색.
+		}
+		
+		
+		
+		// Exception
+		try {
+			System.out.println(testArray[0].concat("      "));
+				
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			System.out.println("=========");
+		} finally {
+			testArray[0] = "";
+			System.out.println("항상 예외처리 후 할일!!!!!");
+			System.out.println("=========");
+		}
+		
+		
+		
+		for(int i = 0; i < testArray.length; i++) {
+			try {
+				System.out.println(testArray[i].concat("     "));
+				
+			} catch (NullPointerException e) {
+				System.out.println(e);
+				System.out.println(e.getMessage());
+			}
+		}
+		
+		
+		try {
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		
+		try {
+			
+		} catch (NullPointerException e) {
+			// TODO: handle exception
+		} catch (NumberFormatException e) {
+			// TODO: handle exception
+		}
+		
+		
+		
+		try {
+			
+		}catch (NullPointerException | NumberFormatException e) {
+			// TODO: handle exception
+		}
+		finally {
+			// TODO: handle finally clause
+		}
+		
+// 2021년 5월 31일. 범위 끝자락		
+		
 		
 		String[] testArray2 = new String[10];  // 아래와 같은형식
 		String testArray3[] = new String[10];  // 위와 같은형식.
