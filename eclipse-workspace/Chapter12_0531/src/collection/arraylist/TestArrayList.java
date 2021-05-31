61f60fda79ce353e6ff59ae357b4517ad97e5f53
SungWoo0315 <noel7752@gmail.com>
2021-05-31 15:42:32 +0900

    20210531

---
+++ eclipse-workspace/Chapter12_0531/src/collection/arraylist/TestArrayList.java
@@ -0,0 +1,27 @@
+package collection.arraylist;
+
+import java.util.ArrayList;
+
+import collection.Member;
+
+public class TestArrayList extends ArrayList<Member> {
+
+	@Override
+	public Member get(int index) {
+		// TODO Auto-generated method stub
+		return super.get(index);
+	}
+	
+	@Override
+	public void add(int index, Member element) {
+		// TODO Auto-generated method stub
+		super.add(index, element);
+	}
+	
+	@Override
+	public Member remove(int index) {
+		// TODO Auto-generated method stub
+		return super.remove(index);
+	}
+	
+}
