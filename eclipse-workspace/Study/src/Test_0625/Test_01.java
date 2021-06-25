5591a2d54963bd58cf07c6411ab03c3f680427b1
SungWoo0315 <noel7752@gmail.com>
2021-06-25 15:22:07 +0900

    20210625

---
+++ eclipse-workspace/Study/src/Test_0625/Test_01.java
@@ -0,0 +1,20 @@
+package Test_0625;
+
+public class Test_01 {
+
+	public static void main(String[] args) {
+		
+			 
+			    int a = 3;
+			    if( a-->4 )
+			      a++;
+			    if( ++a>7 )
+			      ++a;
+			    else
+			      a = ++a + a++;
+			    System.out.println( a );  
+			  
+			
+	}
+
+}
