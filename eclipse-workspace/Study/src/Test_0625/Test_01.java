de12c219980e35d2b038c347fe33b3069bdc6c05
SungWoo0315 <noel7752@gmail.com>
2021-06-25 16:20:53 +0900

    Update Test_01.java

--- eclipse-workspace/Study/src/Test_0625/Test_01.java
+++ eclipse-workspace/Study/src/Test_0625/Test_01.java
@@ -5,16 +5,36 @@ public class Test_01 {
 	public static void main(String[] args) {
 		
 			 
-			    int a = 3;
-			    if( a-->4 )
-			      a++;
-			    if( ++a>7 )
-			      ++a;
-			    else
-			      a = ++a + a++;
-			    System.out.println( a );  
+//			    int a = 3;
+//			    if( a-->4 )
+//			      a++;
+//			    if( ++a>7 )
+//			      ++a;
+//			    else
+//			      a = ++a + a++;
+//			    System.out.println( a );  
 			  
 			
+			    
+//			    int a = 2;
+//			    switch( a ) {
+//			      case 1 : a++;
+//			      case 2 : a++; 
+//			      case 4 : a++;
+//			    }
+//			    System.out.println( a );  
+			    
+			    
+			
+//		   	    int b=1, c=0;
+//		   	    for( int a=2; a<5; a++) {
+//		   	    	if( c++>b ) { b=a+c; }
+//		   	    }
+//		   	    System.out.println( a+b+c );  
+			    	  
+			    	
+			    
+			    
 	}
 
 }
