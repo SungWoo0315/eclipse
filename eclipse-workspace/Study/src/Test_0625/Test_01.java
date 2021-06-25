8ca069024f06106e6e7b871672427fa00c1bae08
SungWoo0315 <noel7752@gmail.com>
2021-06-25 18:10:32 +0900

    Update Test_01.java

--- eclipse-workspace/Study/src/Test_0625/Test_01.java
+++ eclipse-workspace/Study/src/Test_0625/Test_01.java
@@ -42,15 +42,49 @@ public class Test_01 {
 //		      }
 //		      System.out.println( a+b );   
 			    
-	    int a=1, b=5;
-	    jump : for(; ;) {
-	      a++;
-	          for(; ;) {
-	                if(a>--b) break jump;
-	          }
-	    }
-	    System.out.println( a+b );
+//	    int a=1, b=5;
+//	    jump : for(; ;) {
+//	      a++;
+//	          for(; ;) {
+//	                if(a>--b) break jump;
+//	          }
+//	    }
+//	    System.out.println( a+b );
+	    
+	    
+//	    int a=0, b=5;
+//	    outer : for( a=0;a<5;a++ ) {
+//	      inner : for( b=0;b<5;b++) {
+//	            if(a==1&&b==0) { break outer; }
+//	            if(a==2&&b==1) { continue inner; }
+//	      }
+//	    }
+//	    System.out.println( a + b );  
 			    
+	    
+	
+//	    	    int a = 0;
+//	    	    while( a=1 ) {
+//	    	      if(a==3)
+//	    	      break;
+//	    	      a++;
+//	    	    }
+//	    	    System.out.println( a );  
+	    	    
+	    	    
+	    	    
+	    	    
+	    	    
+//	    	    int a = 1, b = 0;
+//	    	    do{
+//	    	      if ( a++ > --b ) continue;  
+//	    	    } while( a < 5 );
+//	    	    System.out.println( a+b ); 
+	    	  
+	    	
+	    
+	    
+	    
 	}
 
 }
