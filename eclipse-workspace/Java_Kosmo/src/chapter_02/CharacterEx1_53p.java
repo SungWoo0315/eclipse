2b0ae7627e278b3c342cf606bbb41790e28a7f4a
SungWoo0315 <noel7752@gmail.com>
2021-07-09 14:55:21 +0900

    20210709
    java 복습

---
+++ eclipse-workspace/Java_Kosmo/src/chapter_02/CharacterEx1_53p.java
@@ -0,0 +1,32 @@
+package chapter_02;
+
+public class CharacterEx1_53p {
+
+	public static void main(String[] args) {
+		char ch1 = 'A';
+		System.out.println(ch1);		// 문자출력
+		System.out.println((int)ch1);	// 문자에 해당하는 정수값(ASCII) 출력
+		
+		char ch2 = 66;					// 정수값 대입
+		System.out.println(ch2);		// 정수값 해당하는 문자 출력
+		
+		int ch3 = 67;
+		System.out.println(ch3);		// 대입한 정수 출력.
+		System.out.println((char)ch3);	// 정수값에 해당하는 문자.
+		
+		
+		// 1분 복습 53쪽
+		
+		char ch4 = 'Z';
+		int ch5 = 38;
+		int ch6 = 97;
+		
+		System.out.println((int)ch4 +  ":"  + (char)ch5 + ":" + (char)ch6);
+		
+		/*
+		 *	형변환으로 각각의 출력값을 다르게 표현. 
+		 */
+
+	}
+
+}
---
+++ eclipse-workspace/Java_Kosmo/src/chapter_02/CharacterEx2_54p.java
@@ -0,0 +1,17 @@
+package chapter_02;
+
+public class CharacterEx2_54p {
+
+	public static void main(String[] args) {
+		char ch1 = '한';			
+		char ch2 = '\uD55C';		// '한' 의 유니코드 값. 16진수, 4비트 사용 = 2바이트 사용.
+		
+		// \ 는 회피시퀀스.
+		
+		
+		System.out.println(ch1);
+		System.out.println(ch2);
+
+	}
+
+}
