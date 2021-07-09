c2cf65c5ed58ce89f48f018e28efa15b5d7bb539
SungWoo0315 <noel7752@gmail.com>
2021-07-09 16:46:22 +0900

    20210709
    java 복습

---
+++ eclipse-workspace/Java_Kosmo/src/chapter_02/ExplicitConversion_68p.java
@@ -0,0 +1,35 @@
+package chapter_02;
+
+public class ExplicitConversion_68p {
+
+	public static void main(String[] args) {
+		
+		/*
+		 * <명시적 형변환> - 66쪽
+		 * 
+		 * 묵시적 형변환과 반대의 경우.
+		 * 
+		 * 바이트 크기가 큰 자료형에서 작은 자료형으로 대입할 경우 쓸 수 있다.
+		 * 강제로 형을 바꾸려면 바꿀 형을 괄호를 써서 명시.
+		 * 
+		 * 자료손실이 발생.
+		 * 
+		 */
+		
+		double dNum = 3.14;
+		int iNum2 = (int)dNum;	// 실수인 double형 변수 dNum에 3.14를 대입하고 초기화. 그 후 정수자료형 int로 형변환하고 변수 iNum2에 대입. 
+		System.out.println(iNum2);	// 강제형변환 후. 자료손실(소수점이하 생략) 발생하고. 정수인 3만 출력.
+		
+		
+		double dNum1 = 1.2;	// 실수 double형 dNum1 변수에 1.2 대입하고 초기화.  
+		float fNum2 = 0.9F;	// float형 fNum2 변수에 0.9F 대입하고 초기화.  
+		
+		int iNum3 = (int)dNum1 + (int)fNum2;	// dNum1변수를 int형으로 강제 형변환, fNum2변수를 int형으로 강제 형변환. 각각 형변환 후 더해짐. (명시적형변환)
+		int iNum4 = (int)(dNum1 + fNum2);	// dNum1 변수와 fNum2 변수를 더한 후 int형으로 강제 형변환. (명시적형변환)
+		
+		System.out.println(iNum3);	// 각각 명시적 형변환 후 dNum1 은 1, fNum2는 0으로 변환된 값을 더한 후 출력.
+		System.out.println(iNum4);	// 우선 dNum1과 fNum2가 묵시적형변환으로 더해진 후. int형으로 명시적 형변환을 함.  
+									// 1.2+0.9 = 2.2 이후 int형으로 형변환하여 결과값은 2가 나온다.
+	}
+
+}
---
+++ eclipse-workspace/Java_Kosmo/src/chapter_02/ImplicitConversion_65p.java
@@ -0,0 +1,43 @@
+package chapter_02;
+
+public class ImplicitConversion_65p {
+
+	public static void main(String[] args) {
+		
+		/*
+		 * <형변환>
+		 * 
+		 * 하나의 자료형으로 통일 한 후 연산을 할 때 '형변환(type conversion)이 이루어 진다.
+		 * 
+		 * 형변환 종류는 
+		 * 1.묵시적형변환(자동형변환) 
+		 * 2.명시적형변환(강제형변환) 이 있다.
+		 * 
+		 * 형 변환의 기본 원칙.
+		 * 1. 바이트 크기가 작은 자료형에서 큰 자료형으로 형 변환은 자동으로 이루어진다.
+		 * 2. 덜 정밀한 자료형에서 더 정밀한 자료형으로 형 변환은 자동으로 이루어진다. 
+		 * 
+		 * 기본원칙과 반대로 형변환이 안되는 이유는 '자료손실' 때문.
+		 * 
+		 */
+		
+		byte bNum = 10;
+		int iNum = bNum;	// byte형 변수 bNum 값을 int형 변수 iNum에 대입함.
+		
+		System.out.println(bNum);
+		System.out.println(iNum);
+		
+		int iNum2 = 20;	//
+		float fNum = iNum2;	// 더 정밀한 float형으로 iNum2가 실수형은 float형으로 변환.
+		
+		System.out.println(iNum);
+		System.out.println(fNum);
+		
+		double dNum;
+		dNum = fNum + iNum;	// 우선적으로 iNum이 float 형으로 형변환 되어 fNum과 연산되고, 연산이 된 후 dNum으로 대입하기 위해 double형으로 변환되어 대입.
+		// 묵시적 형 변환.
+		System.out.println(dNum);
+
+	}
+
+}
