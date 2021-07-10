1ecf94de2eb3919bedf534438366571827fad820
SungWoo0315 <noel7752@gmail.com>
2021-07-10 18:23:31 +0900

    Update p94_IfExample1.java

--- eclipse-workspace/Java_Kosmo/src/chapter_04/p94_IfExample1.java
+++ eclipse-workspace/Java_Kosmo/src/chapter_04/p94_IfExample1.java
@@ -20,8 +20,8 @@ public class p94_IfExample1 {
         if (age >= 8) {                                 // 만약, age값이 8이상이면. (참이면 = true)
             System.out.println("학교에 다닙니다.");     // 이 문장을 수행.
         } 
-        else {                                                // 그렇지 않으면(거짓이면 = false)
-            System.out.println("학교에 다니지 않습니다");     // 이 문장을 수행.
+        else {                                                  // 그렇지 않으면(거짓이면 = false)
+            System.out.println("학교에 다니지 않습니다");       // 이 문장을 수행.
         }
 
         // 결과값은 age변수가 7이므로 8보다 같거나 크지않기 때문에
