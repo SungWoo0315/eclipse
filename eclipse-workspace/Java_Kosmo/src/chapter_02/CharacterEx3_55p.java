4c23e0cfd1e7c8277cba5a221edf8224d7e45ad7
SungWoo0315 <noel7752@gmail.com>
2021-07-10 18:10:08 +0900

    Update CharacterEx3_55p.java

--- eclipse-workspace/Java_Kosmo/src/chapter_02/CharacterEx3_55p.java
+++ eclipse-workspace/Java_Kosmo/src/chapter_02/CharacterEx3_55p.java
@@ -8,7 +8,7 @@ public class CharacterEx3_55p {
         int b = -66;
 
         char a2 = 65;       // 문자형 변수에 숫자 저장.
-//		char b2 = -66;      // char형은 음수값을 표현 할 수 없다.
+//      char b2 = -66;      // char형은 음수값을 표현 할 수 없다.
 
         System.out.println((char) a);
         System.out.println((char) b);   // -66에 해당하는 문자 출력.
