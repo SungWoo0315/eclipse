8622614580914203c4e5840dd30d809c4a8cd854
SungWoo0315 <noel7752@gmail.com>
2021-07-11 12:33:19 +0900

    Create p131_Person.java

---
+++ eclipse-workspace/Java_Kosmo/src/chapter_05/p131_Person.java
@@ -0,0 +1,11 @@
+package chapter_05;
+
+public class p131_Person {
+
+        String name;        // String형 변수 name 선언       // 이름
+        int height;         // int형 변수 height 선언        // 키
+        double weight;      // double형 변수 weight 선언     // 몸무게
+        char gender;        // char형 변수 gender 선언       // 성별
+        boolean married;    // boolean형 변수 married 선언   // 결혼여부
+        
+}
