6874a15fd08f85990add9f07cad0598f46706584
SungWoo0315 <noel7752@gmail.com>
2021-07-11 12:19:35 +0900

    Update p129_Student.java

--- eclipse-workspace/Java_Kosmo/src/chapter_05/p129_Student.java
+++ eclipse-workspace/Java_Kosmo/src/chapter_05/p129_Student.java
@@ -1,9 +1,11 @@
 package chapter_05;
 
-public class p129_Student {
+public class p129_Student {     // 접근제어자 : public 
+                                // 클래스를 만드는 예약어 : class
+                                // 클래스 이름
     
-    int studentID;
-    String studentName;
-    int grade;
-    String address;
+    int studentID;              // int형 변수 studentID(학번)를 선언            // 멤버변수
+    String studentName;         // String형 변수 studentName(학생이름) 선언     // 멤버변수
+    int grade;                  // int형 변수 grade(학년) 선언                  // 멤버변수
+    String address;             // String형 변수 address(사는곳) 선언           // 멤버변수
 }
