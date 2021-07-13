6e52188f9c8ce1d3449b59a4058b01784b5d4518
SungWoo0315 <noel7752@gmail.com>
2021-07-14 01:46:14 +0900

    Update p150_StudentTest2.java

--- eclipse-workspace/Java_Kosmo/src/chapter_05/p150_StudentTest2.java
+++ eclipse-workspace/Java_Kosmo/src/chapter_05/p150_StudentTest2.java
@@ -9,6 +9,18 @@ public class p150_StudentTest2 {
          * 
          * [참조변수]는 [힙메모리]에 생성된 [인스턴스]를 가리킨다.
          * 
+         * [힙메모리]에 생성된 [인스턴스]의 [메모리주소]는 [참조변수]에 저장된다.
+         * =================
+         * <저장 형태> 
+         * 클래스이름@주소값
+         * =================
+         * 주소값 = [해시코드값]
+         * =====================
+         * 객체가 생성되었을 때 생성된 객체에 할당하는 [가상주소값].
+         * 
+         * [참조변수]를 사용하여 [인스턴스]를 참조할 수 있다.
+         * 
+         * 이때 주소값을 [참조값]이라고 한다.
          */
         
         p144_Student student1 = new p144_Student();     // 첫번째 학생 생성         // new예약어를 사용하여 생성자[p144_Student();] 호출하여 새로운 클래스 생성.
@@ -28,8 +40,8 @@ public class p150_StudentTest2 {
         student2.studentName = "안승연";                                            // 참조변수 student2를 사용하여 멤버변수 studentName에 값 "안승연"를 대입.
         
         // 참조변수값 출력.
-        System.out.println(student1);
-        System.out.println(student2);
+        System.out.println(student1);                   // 참조변수 student1 에 저장된 인스턴스 메모리주소. 클래스이름@주소값(해시코드값) 출력.
+        System.out.println(student2);                   // 참조변수 student2 에 저장된 인스턴스 메모리주소. 클래스이름@주소값(해시코드값) 출력.
     }
 
 }
