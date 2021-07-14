ed0b5379f11a153d13a47dc2e8f10aff1d71e381
SungWoo0315 <noel7752@gmail.com>
2021-07-15 02:19:39 +0900

    Create p153_PersonTest.java

---
+++ eclipse-workspace/Java_Kosmo/src/chapter_05/p153_PersonTest.java
@@ -0,0 +1,19 @@
+package chapter_05;
+
+public class p153_PersonTest {
+
+    public static void main(String[] args) {
+       
+        p153_Person personLee = new p153_Person();      // p153_Person() 같은 함수를 [생성자] 라고 한다.
+                                                        
+                                                        /*
+                                                         * 클래스는 멤버 변수는 메서드에 의해 값이 변경될 수도 있지만,
+                                                         * 처음 클래스를 생성 할 때 값을 정해야 하는경우도 있다.
+                                                         * 
+                                                         * [생성자]가 하는일은 클래스를 처음 만들 때 [멤버변수]나 [상수]를 초기화 하는것.
+                                                         * 
+                                                         */
+
+    }
+
+}
