0a92ffc7f776c23e3b07016465f93ebb15dfb8dd
SungWoo0315 <noel7752@gmail.com>
2021-07-15 02:19:41 +0900

    Create p153_Person.java

---
+++ eclipse-workspace/Java_Kosmo/src/chapter_05/p153_Person.java
@@ -0,0 +1,12 @@
+package chapter_05;
+
+// 생성자 테스트를 위한 p153_Person 클래스 생성.  
+
+public class p153_Person {      // 접근제어자 : public 
+                                // 클래스를 만드는 예약어 : class
+                                // 클래스 이름 : p153_Person
+
+        String name;            // String 형 변수 name 선언      //멤버변수
+        float height;           // float 형 변수 height 선언     //멤버변수
+        float weight;           // float 형 변수 weight 선언     //멤버변수
+}
