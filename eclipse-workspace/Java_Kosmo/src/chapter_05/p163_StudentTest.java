0e03de3532237136c59e3046701f3f3ce0bf156a
SungWoo0315 <noel7752@gmail.com>
2021-07-17 19:22:45 +0900

    Update p163_StudentTest.java

--- eclipse-workspace/Java_Kosmo/src/chapter_05/p163_StudentTest.java
+++ eclipse-workspace/Java_Kosmo/src/chapter_05/p163_StudentTest.java
@@ -5,7 +5,8 @@ public class p163_StudentTest {
     public static void main(String[] args) {
         
         p162_Student studentLee = new p162_Student();
-        studentLee.studentName = "이상원";
+//        studentLee.studentName = "이상원";                 // p162_Student 에서 studentName 변수에 [접근제어자] private 예약어를 주어 
+                                                             // 외부 클래스에서 사용 할 수 없어서 오류 발생.
         
         System.out.println(studentLee.getStudnetName());
     }
