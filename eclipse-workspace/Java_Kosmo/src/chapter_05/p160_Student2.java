b57275c601a02fd1fe22ac74963c50512745a252
SungWoo0315 <noel7752@gmail.com>
2021-07-17 16:35:14 +0900

    Create p160_Student2.java

---
+++ eclipse-workspace/Java_Kosmo/src/chapter_05/p160_Student2.java
@@ -0,0 +1,18 @@
+package chapter_05;
+
+public class p160_Student2 {
+
+    
+    int studentID;          // int형 변수 studentID 선언                     // 기본자료형
+    
+    String studentName;     // String형 변수 studentName 선언                // [참조자료형]
+                            // String이 JDK에서 제공하는 [참조 자료형]이다.
+    
+    int koreaScore;         // int형 변수 koreaScore 선언                    // 기본자료형
+    int mathScore;          // int형 변수 mathScore 선언                     // 기본자료형
+    
+    
+    // 과목이름변수 추가.
+    String koreaSubject;    // String형 변수 koreaSubject 선언               // [참조자료형]
+    String mathSubject;     // String형 변수 mathSubject 선언                // [참조자료형]
+}
