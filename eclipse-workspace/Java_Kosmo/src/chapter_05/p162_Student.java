0a2baef9cbd4f6052c023969836232c67faf5955
SungWoo0315 <noel7752@gmail.com>
2021-07-17 19:22:47 +0900

    Update p162_Student.java

--- eclipse-workspace/Java_Kosmo/src/chapter_05/p162_Student.java
+++ eclipse-workspace/Java_Kosmo/src/chapter_05/p162_Student.java
@@ -2,8 +2,21 @@ package chapter_05;
 
 public class p162_Student {
     
+    /*
+     * <접근제어자>
+     * 
+     * 예약어를 사용해 클래스 내부의 변수, 메서드, 생성자에 대한 접근 권한을 지정.
+     * 
+     * public 예약어를 사용하면 외부 클래서에서 접근이 가능하고,
+     * 외부클래스가 사용할 수 있다.
+     * 
+     * private 예약어를 사용하면 외부 클래스에서 사용 할 수 없다.
+     */
+    
     int studentID;
     private String studentName;     // studentName 변수를 private로 선언.
+                                    // p163_StudentTest 클래스에서 사용 할 수 없다.
+    
     int grade;
     String address;
     
