c8f2030bcffcad805acfa3bc8178cb6294f23418
SungWoo0315 <noel7752@gmail.com>
2021-07-17 18:13:21 +0900

    Create p163_StudentTest.java

---
+++ eclipse-workspace/Java_Kosmo/src/chapter_05/p163_StudentTest.java
@@ -0,0 +1,13 @@
+package chapter_05;
+
+public class p163_StudentTest {
+
+    public static void main(String[] args) {
+        
+        p162_Student studentLee = new p162_Student();
+        studentLee.studentName = "이상원";
+        
+        System.out.println(studentLee.getStudnetName());
+    }
+
+}
