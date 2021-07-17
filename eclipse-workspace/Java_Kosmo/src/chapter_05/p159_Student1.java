17196f2cd1e4f0fbc371c22a57d737f0b674b086
SungWoo0315 <noel7752@gmail.com>
2021-07-17 16:35:16 +0900

    Create p159_Student1.java

---
+++ eclipse-workspace/Java_Kosmo/src/chapter_05/p159_Student1.java
@@ -0,0 +1,22 @@
+package chapter_05;
+
+public class p159_Student1 {        
+    
+    /*
+     * <참조 자료형>
+     * 
+     * 크기가 정해진 [기본 자료형](int, char, float, double 등)변수
+     * 
+     * 클래스 자료형으로 선언하는 [참조자료형]변수 
+     * 
+     */
+
+    int studentID;          // int형 변수 studentID 선언                     // 기본자료형
+    
+    String studentName;     // String형 변수 studentName 선언                // [참조자료형]
+                            // String이 JDK에서 제공하는 [참조 자료형]이다.
+    
+    int koreaScore;         // int형 변수 koreaScore 선언                    // 기본자료형
+    int mathScore;          // int형 변수 mathScore 선언                     // 기본자료형
+    
+}
