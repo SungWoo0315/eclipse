189d93115866136ded473dfba146b49b3667b484
SungWoo0315 <noel7752@gmail.com>
2021-07-17 18:13:18 +0900

    Create p161_Subject.java

---
+++ eclipse-workspace/Java_Kosmo/src/chapter_05/p161_Subject.java
@@ -0,0 +1,8 @@
+package chapter_05;
+
+public class p161_Subject {
+
+    String SubjectName;     // 참조자료형 String형 변수 SubjectName 선언  //멤버변수
+    
+    int scorePoint;         // 기본자료형 int 형 변수 scorePoint 선언     //멤버변수
+}
