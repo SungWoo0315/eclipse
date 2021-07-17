1c2d5c00b2ce4cdb98143adb9e53a4bfcab8c3c6
SungWoo0315 <noel7752@gmail.com>
2021-07-17 13:32:29 +0900

    Update p158_PersonTest.java

--- eclipse-workspace/Java_Kosmo/src/chapter_05/p158_PersonTest.java
+++ eclipse-workspace/Java_Kosmo/src/chapter_05/p158_PersonTest.java
@@ -16,14 +16,23 @@ public class p158_PersonTest {
         
         // p157_Person 클래스를 사용하는 p158_PersonTest 클래스에서 구현.
         
+        // 디폴트 생성자를 사용한경우 
+        // 매개변수가 있는 생성자를 사용한 경우.
+        // 두가지를 아래에서 만들었다.
         
-        p157_Person personKim = new p157_Person( );
+        // 디폴트 생성자를 사용한 경우.
+        p157_Person personKim = new p157_Person( );         // new 예약어로 생성자 p157_Person 을 호출하여 새로운 클래스 생성
+                                                            // 클래스형 변수이름(참조변수) personKim 선언하여 new예약어로 생성자를 호출하여 대입.
+                                                            // = (같은뜻)
+                                                            // p157_Person 클래스 자료형으로 참조변수 personKim 선언하고, new p157_Person( ); 로 
+                                                            // p157_Person 클래스를 생성하여 참조변수 personKim 에 대입. 
+                                                            // personKim 은 클래스형 변수이름이자, [참조변수], 이 변수가 생성된 인스턴스를 가리킨다.
+        // 인스턴스 변수값을 대입과 동시에 초기화.
+        personKim.name = "김유신";                           // 인스턴스변수 personKim.name 에 "김유신" 을 대입.        
         
-        personKim.name = "김유신";
-        
-        personKim.weight = 85.5F;
+        personKim.weight = 85.5F;                           // 인스턴스변수 personKim.weight 에 값 85.5F 을 대입. 
                 
-        personKim.height = 180.0F;
+        personKim.height = 180.0F;                          // 인스턴스변수 personKim.height 에 값 180.0F 을 대입. 
         
         
         
