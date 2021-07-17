7ac40cdcd65cf14e7700ad1b33f33476728a5cfc
SungWoo0315 <noel7752@gmail.com>
2021-07-17 18:13:20 +0900

    Create p162_Student.java

---
+++ eclipse-workspace/Java_Kosmo/src/chapter_05/p162_Student.java
@@ -0,0 +1,18 @@
+package chapter_05;
+
+public class p162_Student {
+    
+    int studentID;
+    private String studentName;     // studentName 변수를 private로 선언.
+    int grade;
+    String address;
+    
+    public String getStudnetName() {
+        return studentName;
+    }
+    
+    public void setStudentName(String studnetName) {
+        this.studentName = studentName;
+    }
+
+}
