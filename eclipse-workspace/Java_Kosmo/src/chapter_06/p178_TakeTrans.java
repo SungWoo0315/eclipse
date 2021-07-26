a57f1c57351fca41fa749df5ea615fe3ccab55ff
SungWoo0315 <noel7752@gmail.com>
2021-07-27 00:25:00 +0900

    Update p178_TakeTrans.java

--- eclipse-workspace/Java_Kosmo/src/chapter_06/p178_TakeTrans.java
+++ eclipse-workspace/Java_Kosmo/src/chapter_06/p178_TakeTrans.java
@@ -4,20 +4,31 @@ public class p178_TakeTrans {
 
     public static void main(String[] args) {
         
-        p175_Student studentJames = new p175_Student("James", 5000);
-        p175_Student studentTomas = new p175_Student("Tomas", 10000);
+        p175_Student studentJames = new p175_Student("James", 5000);        // 학생 studentJames 인스턴스 생성.
+                                                                            // 이름 James 가진돈 5000 으로 초기화.
         
-        p177_Bus bus100 = new p177_Bus(100);
-        studentJames.takeBus(bus100);
-        studentJames.showInfo();
-        bus100.showInfo();
+        p175_Student studentTomas = new p175_Student("Tomas", 10000);       // 학생 studentTomas 인스턴스 생성.
+                                                                            // 이름 Tomas 가진돈 10000 으로 초기화.   
+        
+        p177_Bus bus100 = new p177_Bus(100);                       // 버스 bus100 인스턴스 생성.
+        
+        studentJames.takeBus(bus100);                              // takeBus() 메서드 실행.
+        
+        studentJames.showInfo();                                   // showInfo() 메서드 실행.
+                                                                   // studentJames 의 정보 출력.
+        
+        bus100.showInfo();                                         // bus100 의 정보 출력.
 
-        System.out.println();
+        System.out.println();                                      // 한줄 띄우기.
         
-        p177_Subway subwayGreen = new p177_Subway("2호선");
-        studentTomas.takeSubway(subwayGreen);
-        studentTomas.showInfo();
-        subwayGreen.showInfo();
+        p177_Subway subwayGreen = new p177_Subway("2호선");        // 지하철 subwayGreen 인스턴스 생성.
+       
+        studentTomas.takeSubway(subwayGreen);                      // takeSubway() 메서드 실행.
+      
+        studentTomas.showInfo();                                   // showInfo() 메서드 실행
+                                                                   // studentTomas 의 정보 출력.
+       
+        subwayGreen.showInfo();                                    // subwayGreen 의 정보 출력. 
         
     }
 
