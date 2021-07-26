c3b105e810b969632558d1f56b17902e5289d081
SungWoo0315 <noel7752@gmail.com>
2021-07-26 22:41:04 +0900

    Create p175_Student.java

---
+++ eclipse-workspace/Java_Kosmo/src/chapter_06/p175_Student.java
@@ -0,0 +1,29 @@
+package chapter_06;
+
+public class p175_Student {
+
+    public String studentName;
+    public int grade;
+    public int money;
+    
+    public p175_Student(String studentName, int money) {
+        this.studentName = studentName;
+        this.money = money;
+    }
+    
+    public void takeBus(Bus bus) {
+        bus.take(1000);
+        this.money -= 1000;
+    }
+    
+    public void takeSubway(Subway subway) {
+        subway.take(1500);
+        this.money -= 1500;
+    }
+    
+    public void showInfo() {
+        System.out.println(studentName + "님의 남은돈은" + money + "입니다.");
+    }
+    
+    
+}
