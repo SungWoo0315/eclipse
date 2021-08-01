e0d53428447b145b33eaa287c3052bc4f91f2843
SungWoo0315 <noel7752@gmail.com>
2021-08-01 18:45:30 +0900

    Create p208_BookArray.java

---
+++ eclipse-workspace/Java_Kosmo/src/chapter_07/p208_BookArray.java
@@ -0,0 +1,15 @@
+package chapter_07;
+
+public class p208_BookArray {
+
+    public static void main(String[] args) {
+
+
+        p207_Book[] library = new p207_Book[5];
+        
+        for (int i = 0; i < library.length; i++) {
+            System.out.println(library[i]);
+        }
+    }
+
+}
