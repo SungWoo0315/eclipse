b3dab08c403f4e89f0f6ceae562f0d7299df4318
SungWoo0315 <noel7752@gmail.com>
2021-08-01 17:15:50 +0900

    Update p206_CharArray.java

--- eclipse-workspace/Java_Kosmo/src/chapter_07/p206_CharArray.java
+++ eclipse-workspace/Java_Kosmo/src/chapter_07/p206_CharArray.java
@@ -18,7 +18,6 @@ public class p206_CharArray {
              * 각 알파벳 문자는 실제 메모리에 아스키 코드값으로 저장된다.
              * 
              * ch 값에 1을 증가시키면 1증가한 아스키 코드값이 배열에 저장된다.
-             * 
              */
             
             
@@ -28,9 +27,12 @@ public class p206_CharArray {
         
         
         
-        for (int i = 0; i < alphabets.length; i++) {                        // 
+        for (int i = 0; i < alphabets.length; i++) {                        // 반복문으로 alphabets 배열의 길이만큼 
+                                                                            // i 1씩 증가해서 반복.
             
-            System.out.println(alphabets[i] + "," + (int)alphabets[i]);
+            System.out.println(alphabets[i] + "," + (int)alphabets[i]);     // 배열 alphabets 반복하면서 출력, + 
+                                                                            // 형변환 연산자 (int) 사용하여 배열에 저장된,
+                                                                            // char 형 문자를 int 형 정수로 변환해서 출력.  
             
         }
     }
