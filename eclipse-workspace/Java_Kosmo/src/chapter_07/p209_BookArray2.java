436c4b116600c3e6791877054a6ffeafc4d9b80c
SungWoo0315 <noel7752@gmail.com>
2021-08-01 19:49:23 +0900

    Update p209_BookArray2.java

--- eclipse-workspace/Java_Kosmo/src/chapter_07/p209_BookArray2.java
+++ eclipse-workspace/Java_Kosmo/src/chapter_07/p209_BookArray2.java
@@ -7,7 +7,9 @@ public class p209_BookArray2 {
         p207_Book[] library = new p207_Book[5];         // p207_Book 클래스형으로 library 객체배열 생성.  
         
         
-        // 인스턴스 생성 후, 각 배열에 저장.  
+        /* 인스턴스 생성 후, 각 배열에 저장.  
+         * 배열의 각 요소에 p207_Book 인스턴스를 만들어 저장.
+         */
         
         library[0] = new p207_Book("태백산맥", "조정래");
         library[1] = new p207_Book("데미안", "헤르만 헤세");
@@ -17,13 +19,15 @@ public class p209_BookArray2 {
 
         for (int i = 0; i < library.length; i++) {      // 반복문으로 library 길이만큼 반복.
 
-            library[i].showBookInfo();  
+            library[i].showBookInfo();              // 반복문으로 책 이름과 저자를 출력하는 showBookInfo() 메서드 실행.
         }
         
             System.out.println("---------------------------");  // 행 구분 처리.  
         
-        for (int i = 0; i < library.length; i++) {
-            System.out.println(library[i]);
+        for (int i = 0; i < library.length; i++) {      // 반복문으로 library 길이만큼 반복.
+            
+            System.out.println(library[i]);         // library[i] 출력, 각 요소에는 p207_Book 인스턴스 주소값이 저장되어 있다.
+                                                    // 각 인스턴스 주소값이 출력된다. 
         }
 
     }
