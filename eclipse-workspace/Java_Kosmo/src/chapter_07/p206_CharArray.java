1f9a7ab5d856b81cab270770aa839d9df4928dec
SungWoo0315 <noel7752@gmail.com>
2021-08-01 16:12:22 +0900

    Create p206_CharArray.java

---
+++ eclipse-workspace/Java_Kosmo/src/chapter_07/p206_CharArray.java
@@ -0,0 +1,29 @@
+package chapter_07;
+
+public class p206_CharArray {
+
+    public static void main(String[] args) {
+
+
+        char[] alphabets = new char[26];
+        
+        char ch = 'A';
+        
+        
+        
+        for (int i = 0; i < alphabets.length; i++, ch++) {
+            
+            alphabets[i] = ch;
+            
+        }
+        
+        
+        
+        for (int i = 0; i < alphabets.length; i++) {
+            
+            System.out.println(alphabets[i] + "," + (int)alphabets[i]);
+            
+        }
+    }
+
+}
